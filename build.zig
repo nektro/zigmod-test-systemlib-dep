450ca46c0fb951e0857b74857445b9595ef1803a
Meghan Denny <hello@nektro.net>
2026-06-01 21:25:18 -0700

    update to zig 0.15.2

--- build.zig
+++ build.zig
@@ -6,9 +6,11 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     deps.addAllTo(exe_unit_tests);
 
--- main.zig
+++ main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
+    var list = std.array_list.Managed(i32).init(std.testing.allocator);
     defer list.deinit();
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
